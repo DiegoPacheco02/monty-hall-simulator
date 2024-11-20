f43322b1edb5be8dd83bd22d7992217bbb46c6d9
Erika Hernández <62717848+E342@users.noreply.github.com>
2024-11-19 20:24:46 -0600

    Actualizando reglas del juego

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -46,19 +46,22 @@ const Footer = () => {
           <div className="dialog-content">
             <h2>Reglas del Juego Monty Hall</h2>
             <p>
-              - El objetivo de este juego es elegir la puerta ganadora entre tres puertas disponibles. 
-              Detrás de la puerta ganadora hay un auto nuevo, y detrás de las otras dos puertas hay cabras.
+              Tu objetivo es elegir la puerta ganadora entre tres puertas disponibles. Detrás de la puerta ganadora
+              está tu graduación asegurada, mientras que detrás de las otras dos puertas hay búhos.
             </p>
+            <ul>
+              <li>
+                Primero, debes escoger una de las tres puertas.
+              </li>
+              <li>
+                Luego, se revelará un búho detrás de una de las otras dos puertas que no elegiste.
+              </li>
+              <li>
+                Finalmente, tendrás la opción de mantener tu elección inicial o cambiarte a la otra puerta cerrada.
+              </li>
+            </ul>
             <p>
-              - Primero, el jugador debe escoger una de las tres puertas.
-            </p>
-            <p>
-              - Luego, el presentador del programa revelará una cabra detrás de una de las otras dos puertas 
-              (el presentador siempre sabe dónde está el auto y nunca lo revelará).
-            </p>
-            <p>
-              - Finalmente, el jugador puede abrir la puerta que eligió inicialmente o cambiarse a la otra puerta cerrada. 
-              ¡Si la puerta que abre revela un auto nuevo, el jugador gana!
+              Si abres la puerta que lleva a tu graduación, ¡felicidades, ganaste!
             </p>
             <button onClick={toggleRules}>Cerrar</button>
           </div>
